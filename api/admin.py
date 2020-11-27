4646c114de043bedccbfbb8bc8cbc30ad1f2a837
Francisco Badilla <f.badilla228@gmail.com>
2020-11-27 12:45:12 +0000

    Cambios en admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-from .models import CasosEnviados
+from .models import CasosEnviados , CallCenters ,Comunas , Estados, CasosRecibiados, CasosComunas, CasosEstado
 admin.site.register(CasosEnviados)
+admin.site.register(CallCenters)
+admin.site.register(Comunas)
+admin.site.register(Estados)
+admin.site.register(CasosRecibiados)
+admin.site.register(CasosComunas)
+admin.site.register(CasosEstado)
